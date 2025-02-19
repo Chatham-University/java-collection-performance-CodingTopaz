59e0deacbfd31e88210e7fe9997496e3be302282
CodingTopaz <geniemalkin@gmail.com>
2025-02-19 16:18:40 -0500

    Initial Commit to C#
    Initial Commit

---
+++ C#/CollectionDemo/CollectionDemo/CollectionTest.cs
@@ -0,0 +1,14 @@
+﻿
+public class CollectionTest
+{
+    public static int How_Many_Nums = (int)Math.Pow(10, 7);
+
+    public static void Main(string[] args)
+    { 
+       
+           
+       }
+    }
+
+
+     
