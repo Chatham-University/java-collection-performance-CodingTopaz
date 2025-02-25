b403c7ec00b6072d714e479105fdc98055cecbe8
CodingTopaz <geniemalkin@gmail.com>
2025-02-25 11:56:44 -0500

    Array List
    This creates the code for the ArrayList

---
+++ C#/CollectionChallengeforGit/CollectionChallenge/CollectionChallenge/VectorDemo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Globalization;
+
+namespace CollectionChallenge;
+
+public class ArrayListVectorDemo
+{
+    private ArrayList _vector; 
+    
+
+    public ArrayListVectorDemo(int howManyNums, Random rand)
+    {
+        _vector = new ArrayList(new int[howManyNums]);
+        for (int i = 0; i < howManyNums; i++)
+        {
+            howManyNums.CompareTo(rand.Next());
+        }
+    }
+}
+        
+     
