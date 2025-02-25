55a00e63f358155573878f004f83b6a95a97d1f0
CodingTopaz <geniemalkin@gmail.com>
2025-02-25 15:53:20 -0500

    Linked List and Vector Demo
    This fixed up the code for these two data types with guidance from Lou.

--- C#/CollectionChallengeforGit/CollectionChallenge/CollectionChallenge/LinkedListDemo.cs
+++ C#/CollectionChallengeforGit/CollectionChallenge/CollectionChallenge/LinkedListDemo.cs
@@ -2,13 +2,13 @@
 
 public class LinkedListDemo
 {
-    private LinkedList<int> nums;
-    public LinkedListDemo(int howManyNums, Random rand)
+    private LinkedList<int> nums; //original empty linked list
+    public LinkedListDemo(int howManyNums/*how many loops*/, Random rand)
     {
-        nums = new LinkedList<int>();
-        for (int i = 0; i < howManyNums; i++)
+        nums = new LinkedList<int>(); // new linked list
+        for (int i = 0; i < howManyNums; i++) // for i in range(4)
         {
-            nums.AddLast(rand.Next(howManyNums));
+            nums.AddLast(rand.Next(howManyNums)); //add a random element into the new test
         }
     }
 }
--- C#/CollectionChallengeforGit/CollectionChallenge/CollectionChallenge/VectorDemo.cs
+++ C#/CollectionChallengeforGit/CollectionChallenge/CollectionChallenge/VectorDemo.cs
@@ -10,10 +10,10 @@ public class ArrayListVectorDemo
 
     public ArrayListVectorDemo(int howManyNums, Random rand)
     {
-        _vector = new ArrayList(new int[howManyNums]);
+        _vector = new ArrayList();
         for (int i = 0; i < howManyNums; i++)
         {
-            howManyNums.CompareTo(rand.Next());
+            _vector.Add(rand.Next(howManyNums));
         }
     }
 }
