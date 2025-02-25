5d9521623fe4c6f4e22c8bed799aff7a1b97b054
CodingTopaz <geniemalkin@gmail.com>
2025-02-25 12:01:53 -0500

    Array Demo File
    This creates the Array Demo.

---
+++ C#/CollectionChallengeforGit/CollectionChallenge/CollectionChallenge/ArrayDemo.cs
@@ -0,0 +1,16 @@
+﻿namespace CollectionChallenge;
+
+public class ArrayDemo
+{
+    private int[] nums;
+
+    public ArrayDemo(int numscount, Random rand)
+    {
+        this.nums = new int[numscount];
+        for (int i = 0; i < numscount; i++)
+        {
+            this.nums[i] = rand.Next(numscount);
+        }
+        
+    }
+}
