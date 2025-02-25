a4039ba77c444efe507bc0ff43dac0b85bf24923
CodingTopaz <geniemalkin@gmail.com>
2025-02-25 12:05:31 -0500

    List Demo
    This is the code for the List Demo

---
+++ C#/CollectionChallengeforGit/CollectionChallenge/CollectionChallenge/ListDemo.cs
@@ -0,0 +1,15 @@
+﻿namespace CollectionChallenge;
+
+public class ListDemo
+{
+    private List<int> numbers;
+    
+    public ListDemo(int howmany,Random random)
+    {
+        numbers = new List<int>();
+        for (int i = 0; i < howmany; i++)
+        {
+            numbers.Add(random.Next(howmany));
+        }
+    }
+}
