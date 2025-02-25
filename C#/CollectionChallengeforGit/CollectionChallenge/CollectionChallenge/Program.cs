dc1f01edef3e789410d5287294778df6c8684940
CodingTopaz <geniemalkin@gmail.com>
2025-02-25 11:59:40 -0500

    Main File
    This runs the CollectionDemos file.

---
+++ C#/CollectionChallengeforGit/CollectionChallenge/CollectionChallenge/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Collections;
+using System.Diagnostics;
+using CollectionChallenge;
+using System.Numerics;
+
+Stopwatch stopwatch = new Stopwatch();
+
+Random rand = new Random();
+int num =  (int)Math.Pow(10, 5);
+stopwatch.Start();
+var arraydemo = new ArrayDemo(num,rand);
+stopwatch.Stop();
+Console.WriteLine("did arraydemo in "+stopwatch.Elapsed);
+
+stopwatch.Restart();
+var linkedListDemo = new LinkedListDemo(num, rand);
+stopwatch.Stop();
+Console.WriteLine("did linkedlistdemo in "+stopwatch.Elapsed);
+
+
+
+var regularListDemo = new ListDemo(num, rand);
+ListDemo(num, rand);
+stopwatch.Stop();
+Console.WriteLine("did regular list in "+stopwatch.Elapsed);
+
+var ArrayListVectorDemo = new ArrayListVectorDemo(num, rand);
+stopwatch.Stop();
+Console.WriteLine("did ArrayList in "+stopwatch.Elapsed);
+
+void ListDemo(int howmany, Random random)
+{
+    List<int> numbers = new List<int>();
+    for (int i = 0; i < howmany; i++)
+    {
+        numbers.Add(random.Next(howmany));
+    }
+}
+
+    
+    
