706ab7a09a53f3b70e519d8cbf6dc4bcd79145f4
CodingTopaz <geniemalkin@gmail.com>
2025-02-25 12:03:51 -0500

    Linked List Demo
    This is the code for the Linked List Demo.

---
+++ C#/CollectionChallengeforGit/CollectionChallenge/CollectionChallenge/LinkedListDemo.cs
@@ -0,0 +1,14 @@
+﻿namespace CollectionChallenge;
+
+public class LinkedListDemo
+{
+    private LinkedList<int> nums;
+    public LinkedListDemo(int howManyNums, Random rand)
+    {
+        nums = new LinkedList<int>();
+        for (int i = 0; i < howManyNums; i++)
+        {
+            nums.AddLast(rand.Next(howManyNums));
+        }
+    }
+}
